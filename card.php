3539e05d04cb63c8890fd8c61496e7a7c15f715e
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-02 23:11:03 +0200

    - through include card.php in index.php is $row[] in card.php available - title, description, price from row[]   -> possible through db result fetch in index.php

--- card.php
+++ card.php
@@ -1,10 +1,13 @@
 <div class="card">
-    <img src="http://placekitten.com/286/180" class="card-img-top" alt="Produktbild">
+    <div class="card-title"><?= $row['title'] ?></div>
+    <img src="https://placekitten.com/286/180" class="card-img-top" alt="Produktbild">
     <div class="card-body">
-        <p>Diese tollle Katze ist der Hit.</p>
+        <p><?= $row['description'] ?></p>
+        <hr>
+        <?= $row['price'] ?>
     </div>
     <div class="card-footer">
-        <a ref="" class="btn btn-primary btn-sm">Details</a>
-        <a ref="" class="btn btn-success btn-sm">In den Warenkorb</a>
+        <a href="" class="btn btn-primary btn-sm">Details</a>
+        <a href="" class="btn btn-success btn-sm">In den Warenkorb</a>
     </div>
 </div>
