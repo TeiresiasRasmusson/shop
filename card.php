35d9f1ca5d7479483f3a753982dfd657f0565382
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-02 20:14:28 +0200

    PHP Online Shop - Part 1 - index.php erstellt - bootstrap (css, js) eingebunden - style.css erstellt und eingebunden - card Bereich in eigene Datei ausgelagert (card.php) und included

---
+++ card.php
@@ -0,0 +1,10 @@
+<div class="card">
+    <img src="http://placekitten.com/286/180" class="card-img-top" alt="Produktbild">
+    <div class="card-body">
+        <p>Diese tollle Katze ist der Hit.</p>
+    </div>
+    <div class="card-footer">
+        <a ref="" class="btn btn-primary btn-sm">Details</a>
+        <a ref="" class="btn btn-success btn-sm">In den Warenkorb</a>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,5 +1,39 @@
 <?php
 error_reporting(-1);
 ini_set('display_errors','On');
+?>
 
-echo "Das ist ein Test";
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <title>Kitten Webshop</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+<header class="bg-light p-5 rounded-lg m-3">
+    <div class="container">
+        <h1>Willkommen im Kitten Webshop</h1>
+    </div>
+</header>
+<section class="container" id="content">
+    <div class="row">
+        <div class="col">
+            <?php include 'card.php'?>
+        </div>
+        <div class="col">
+            <?php include 'card.php'?>
+        </div>
+        <div class="col">
+            <?php include 'card.php'?>
+        </div>
+        <div class="col">
+            <?php include 'card.php'?>
+        </div>
+    </div>
+
+</section>
+<script src="assets/js/bootstrap.bundle.js"></script>
+</body>
+</html>
