8a9cd7d5024edb6c9ebedec721dab011a3022067
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-06 13:42:25 +0200

    Initial Commit

---
+++ config/database.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_DATABASE','shop');
+define('DB_USERNAME','asmus');
+define('DB_PASSWORD','asmus');
+define('DB_HOST','database');
+define('DB_CHARSET','utf8mb4');
+
---
+++ templates/main.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <title>Kitten Webshop</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./assets/css/bootstrap.css">
+    <link rel="stylesheet" href="./assets/css/styles.css">
+</head>
+<body>
+<header class="bg-light p-5 rounded-lg m-3">
+    <div class="container">
+        <h1>Willkommen im Kitten Webshop</h1>
+    </div>
+</header>
+<section class="container" id="content">
+    <div class="row">
+        <?php while ($row = $result->fetch()):?>
+            <div class="col">
+                <?php include 'card.php' ?>
+            </div>
+        <?php endwhile;?>
+    </div>
+</section>
+<script src="./assets/js/bootstrap.bundle.js"></script>
+</body>
+</html>
