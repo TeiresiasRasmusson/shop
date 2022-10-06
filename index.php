24918ec083604b7f869b5cb727be811996586bb2
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-06 13:50:51 +0200

    PHP Online Shop Part 3 by Vitaly Mic index.php aufgeräumt und vieles ausgelagert

--- index.php
+++ index.php
@@ -2,40 +2,11 @@
 error_reporting(-1);
 ini_set('display_errors','On');
 
-$username = "asmus";
-$password = "asmus";
-$dsn = "mysql:host=mariadb;dbname=shop;charset=utf8mb4";
-$db = new PDO($dsn,$username,$password);
+define('CONFIG_DIR',__DIR__.'/config');
+require_once __DIR__.'/functions/database.php';
 
 $sql = "SELECT id,title,description,price FROM products";
 
-$result = $db->query($sql);
+$result = getDB()->query($sql);
 
-?>
-
-<!DOCTYPE html>
-<html lang="de">
-<head>
-    <title>Kitten Webshop</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
-</head>
-<body>
-<header class="bg-light p-5 rounded-lg m-3">
-    <div class="container">
-        <h1>Willkommen im Kitten Webshop</h1>
-    </div>
-</header>
-<section class="container" id="content">
-    <div class="row">
-        <?php while ($row = $result->fetch()):?>
-            <div class="col">
-                <?php include 'card.php'?>
-            </div>
-        <?php endwhile;?>
-    </div>
-</section>
-<script src="assets/js/bootstrap.bundle.js"></script>
-</body>
-</html>
+require __DIR__.'/templates/main.php';
