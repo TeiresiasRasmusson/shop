34fc71f97189d0eb2574faec731d52b3143da4a0
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-06 15:58:48 +0200

    PHP Online Shop Part 4 by Vitaly Mic navbar template includet

--- templates/main.php
+++ templates/main.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
+<?php include __DIR__.'/navbar.php'?>
 <header class="bg-light p-5 rounded-lg m-3">
     <div class="container">
         <h1>Willkommen im Kitten Webshop</h1>
