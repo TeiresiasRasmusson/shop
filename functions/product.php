f3d3fbbec7dd61103a5b94df4e4f5bb30857182d
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-07 18:06:39 +0200

    Inital Commit

--- functions/product.php
+++ functions/product.php
@@ -1,1 +1,18 @@
 <?php
+
+function getAllProducts(){
+
+    $sql = "SELECT id, title, description, price FROM products";
+
+    $result = getDB()->query($sql);
+    if(!$result){
+        return [];
+    }
+
+    $products = [];
+    while ($row = $result->fetch()){
+        $products[] = $row;
+    }
+
+    return $products;
+}
--- templates/card.php
+++ templates/card.php
@@ -1,13 +1,13 @@
 <div class="card">
-    <div class="card-title"><?= $row['title'] ?></div>
-    <img src="https://placekitten.com/286/180?image=<?= $row['id'] ?>" class="card-img-top" alt="Produktbild">
+    <div class="card-title"><?= $product['title'] ?></div>
+    <img src="https://placekitten.com/286/180?image=<?= $product['id'] ?>" class="card-img-top" alt="Produktbild">
     <div class="card-body">
-        <p><?= $row['description'] ?></p>
+        <p><?= $product['description'] ?></p>
         <hr>
-        <?= $row['price'] ?>
+        <?= $product['price'] ?>
     </div>
     <div class="card-footer">
         <a href="" class="btn btn-primary btn-sm">Details</a>
-        <a href="index.php/cart/add/<?= $row['id'] ?>" class="btn btn-success btn-sm">In den Warenkorb</a>
+        <a href="index.php/cart/add/<?= $product['id'] ?>" class="btn btn-success btn-sm">In den Warenkorb</a>
     </div>
 </div>
