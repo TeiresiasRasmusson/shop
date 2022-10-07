a0a61191d1ea6972bde9e49f2872f342f381b62f
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-07 16:52:01 +0200

    PHP Online Shop Part 5 by Vitalij Mic
    - $userId wird generiert
    - userId wird in Cookie gespeichert
    - route wird aus $_SERVER['REQUEST_URI'] gebaut
    - wenn route == /cart/add/ packe product in db.shop.cart

--- index.php
+++ index.php
@@ -10,14 +10,35 @@ $sql = "SELECT id,title,description,price FROM products";
 
 $result = getDB()->query($sql);
 
-$userId = 1337;
 $cartItems = 0;
+$userId = random_int(0,time());
 
+if(isset($_COOKIE['userId'])){
+    $userId = (int) $_COOKIE['userId'];
+}
 if(isset($_SESSION['userId'])){
     $userId = (int) $_SESSION['userId'];
 }
-if(isset($_COOKIE['userId'])){
-    $userId = (int) $_COOKIE['userId'];
+setcookie('userId',$userId,strtotime('+30 days'));
+
+$url = $_SERVER['REQUEST_URI'];
+$positionInUrl = strpos($url,'index.php');
+$route = substr($url,$positionInUrl);
+$route = str_replace('index.php','',$route);
+
+if(strpos($route,'/cart/add/') !== false){
+    $routeParts = explode('/',$route);
+    $productId = (int) $routeParts[3];
+
+    $sql = "INSERT INTO cart SET user_id = :userId,product_id = :productId";
+    $statement = getDB()->prepare($sql);
+
+    $statement->execute([
+        ':userId'=>$userId,
+        ':productId'=>$productId
+    ]);
+    header("Location: /shop/index.php");
+    exit();
 }
 
 $sql = "SELECT COUNT(id) FROM cart WHERE user_id =".$userId;
