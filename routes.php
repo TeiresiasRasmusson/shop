fc2e485b0571faa5bc5665a920d826a2184d7ad2
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-07 21:57:21 +0200

    PHP Online Shop Part 7 by Vitalij Mic
    - route-Logic revised, when no index.php in URL (domain base)
    - add route for cart

--- routes.php
+++ routes.php
@@ -1,17 +1,38 @@
 <?php
 $url = $_SERVER['REQUEST_URI'];
-$positionInUrl = strpos($url,'index.php');
-$route = substr($url, $positionInUrl);
-$route = str_replace('index.php','', $route);
+$indexPHPPositionInUrl = strpos($url,'index.php');
+$baseUrl = substr($url, 0, $indexPHPPositionInUrl);
+
+$route =  0;
+
+if(false !== $indexPHPPositionInUrl){
+
+    $route = substr($url, $indexPHPPositionInUrl);
+    $route = str_replace('index.php','', $route);
+}
+
+$userId = getCurrentUserId();
+$countCartItems = countProductsInCart($userId);
+
+if(!$route){
+
+    $products = getAllProducts();
+    require __DIR__.'/templates/main.php';
+    exit();
+}
 
 if(strpos($route,'/cart/add/') !== false){
+
     $routeParts = explode('/',$route);
-
-    $userId = getCurrentUserId();
     $productId = (int) $routeParts[3];
-
     addProductToCart($userId, $productId);
+    header("Location: ".$baseUrl."index.php");
+    exit();
+}
 
-    header("Location: /shop/index.php");
+if(strpos($route,'/cart') !== false){
+
+    $cartItems = getCartItemsForUserId($userId);
+    require __DIR__.'/templates/cartPage.php';
     exit();
 }
