7f92085862b4af8e27afe89b66b0ab7c2aa70426
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-07 22:07:10 +0200

    PHP Online Shop Part 7 by Vitalij Mic
    - add cartItems snippet includet in cartPage.php

--- templates/cartItem.php
+++ templates/cartItem.php
@@ -1,1 +1,8 @@
-<?php
+<div class="col-4">
+    <img src="https://placekitten.com/286/180?image=<?= $cartItem['product_id'] ?>" class="card-img-top" alt="Produktbild-<?= $cartItem['product_id'] ?>">
+</div>
+<div class="col-8">
+    <div><?= $cartItem['title'] ?></div>
+    <div><?= $cartItem['description'] ?></div>
+    <div><?= $cartItem['price'] ?></div>
+</div>
