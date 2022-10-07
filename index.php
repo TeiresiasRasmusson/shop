faf521cf580d400aa7e485d57ee7569b53eca306
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-07 13:32:58 +0200

    PHP Online Shop Part 4 by Vitaly Mic _SESSION und _COOKIE aktiviert mit session_start() db fetch COUNT(id) in db.shop.cart über $userId
    For most databases, PDOStatement::rowCount() does not return the number of rows affected by a SELECT statement. Instead, use PDO::query() to issue a SELECT COUNT(*) statement with the same predicates as your intended SELECT statement, then use PDOStatement::fetchColumn() to retrieve the number of matching rows.

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 error_reporting(-1);
 ini_set('display_errors','On');
 
@@ -8,5 +9,19 @@ require_once __DIR__.'/functions/database.php';
 $sql = "SELECT id,title,description,price FROM products";
 
 $result = getDB()->query($sql);
+
+$userId = 1337;
+$cartItems = 0;
+
+if(isset($_SESSION['userId'])){
+    $userId = (int) $_SESSION['userId'];
+}
+if(isset($_COOKIE['userId'])){
+    $userId = (int) $_COOKIE['userId'];
+}
+
+$sql = "SELECT COUNT(id) FROM cart WHERE user_id =".$userId;
+$cartResults = getDB()->query($sql);
+$cartItems = $cartResults->fetchColumn();
 
 require __DIR__.'/templates/main.php';
