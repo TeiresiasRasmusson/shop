36c115d68ae025dfb28bf5b6a05a896aa5d8adad
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-08 00:02:43 +0200

    PHP Online Shop Part 8 by Vitalij Mic
    - styling adjusttments
    - add some summs
    - add number_format()

--- templates/cartPage.php
+++ templates/cartPage.php
@@ -9,17 +9,33 @@
 </head>
 <body>
 <?php include __DIR__.'/navbar.php'?>
-<header class="bg-light pt-5 pb-5">
+<header class="bg-light pt-5 pb-5 mb-4">
     <div class="container">
         <h1>Willkommen im Kitten Webshop</h1>
     </div>
 </header>
 <section class="container" id="cartItems">
-    <?php foreach($cartItems as $cartItem):?>
     <div class="row">
+        <h2>Warenkorb</h2>
+    </div>
+    <div class="row cartItemHeader">
+        <div class="col-12 text-end">
+            Preis
+        </div>
+    </div>
+    <?php foreach($cartItems as $cartItem):?>
+    <div class="row cartItem">
         <?php include __DIR__.'/cartItem.php' ?>
     </div>
     <?php endforeach;?>
+    <div class="row">
+        <div class="col-12 text-end">
+            Gesamtpreis (<?= $countCartItems ?> Artikel): <span class="price"><?= number_format($cartSum/100,2,","," ") ?> €</span>
+        </div>
+    </div>
+    <div class="row">
+        <a class="btn btn-primary col-12" href="#">Zur Kasse gehen</a>
+    </div>
 </section>
 <script src="assets/js/bootstrap.bundle.js"></script>
 </body>
