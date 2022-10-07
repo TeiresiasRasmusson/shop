89adbbdb4196358c1bdd8aece2c5284a86d35c72
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-07 18:20:43 +0200

    PHP Online Shop Part 6 by Vitalij Mic
    - ausgelagertes route-handling von index.php eingefügt

--- routes.php
+++ routes.php
@@ -1,1 +1,15 @@
 <?php
+$url = $_SERVER['REQUEST_URI'];
+$positionInUrl = strpos($url,'index.php');
+$route = substr($url, $positionInUrl);
+$route = str_replace('index.php','', $route);
+
+if(strpos($route,'/cart/add/') !== false){
+    $routeParts = explode('/',$route);
+    $productId = (int) $routeParts[3];
+
+    addProductToCart($userId, $productId);
+
+    header("Location: /shop/index.php");
+    exit();
+}
