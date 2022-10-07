ca05eede76a49adbde3b20aa8c96a3a5ef17ef36
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-07 18:27:23 +0200

    PHP Online Shop Part 6 by Vitalij Mic
    - refactoring
    - includes in includes.php ausgelagert
    - userId setzen in functions/user.php ausgelagert
    - countProductsInCart() in functions/cart.php ausgelagert
    - route-handling in routes.php ausgelagert

--- index.php
+++ index.php
@@ -4,45 +4,13 @@ error_reporting(-1);
 ini_set('display_errors','On');
 
 define('CONFIG_DIR',__DIR__.'/config');
-require_once __DIR__.'/functions/database.php';
+require_once __DIR__.'/includes.php';
 
-$sql = "SELECT id,title,description,price FROM products";
+$userId = getCurrentUserId();
+$products = getAllProducts();
 
-$result = getDB()->query($sql);
+setcookie('userId', $userId, strtotime('+30 days'));
 
-$cartItems = 0;
-$userId = random_int(0,time());
-
-if(isset($_COOKIE['userId'])){
-    $userId = (int) $_COOKIE['userId'];
-}
-if(isset($_SESSION['userId'])){
-    $userId = (int) $_SESSION['userId'];
-}
-setcookie('userId',$userId,strtotime('+30 days'));
-
-$url = $_SERVER['REQUEST_URI'];
-$positionInUrl = strpos($url,'index.php');
-$route = substr($url,$positionInUrl);
-$route = str_replace('index.php','',$route);
-
-if(strpos($route,'/cart/add/') !== false){
-    $routeParts = explode('/',$route);
-    $productId = (int) $routeParts[3];
-
-    $sql = "INSERT INTO cart SET user_id = :userId,product_id = :productId";
-    $statement = getDB()->prepare($sql);
-
-    $statement->execute([
-        ':userId'=>$userId,
-        ':productId'=>$productId
-    ]);
-    header("Location: /shop/index.php");
-    exit();
-}
-
-$sql = "SELECT COUNT(id) FROM cart WHERE user_id =".$userId;
-$cartResults = getDB()->query($sql);
-$cartItems = $cartResults->fetchColumn();
+$cartItems = countProductsInCart($userId);
 
 require __DIR__.'/templates/main.php';
