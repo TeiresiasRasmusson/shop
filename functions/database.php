53b1aa1b4c2f836767501ce6b54072bf5d337591
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-07 18:16:30 +0200

    PHP Online Shop Part 6 by Vitalij Mic
    - printDBErrorMessages() hinzugefügt, DB Fehleranalyse var_dump()

--- functions/database.php
+++ functions/database.php
@@ -1,6 +1,7 @@
 <?php
 
 function getDB(){
+
     static $db;
     if ($db instanceof PDO){
         return $db;
@@ -9,4 +10,13 @@ function getDB(){
     $dsn = sprintf("mysql:host=%s;dbname=%s;charset=%s",DB_HOST, DB_DATABASE, DB_CHARSET);
     $db = new PDO($dsn,DB_USERNAME,DB_PASSWORD);
     return $db;
+}
+
+function printDBErrorMessage (){
+
+    $errorMessage = getDB()->errorInfo();
+    if(isset($errorMessage[2])){
+        return $errorMessage[2];
+    }
+    return '';
 }
