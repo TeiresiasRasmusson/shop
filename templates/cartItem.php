89264832e86ce1444610f4c709680ed037b78019
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-08 00:04:02 +0200

    PHP Online Shop Part 8 by Vitalij Mic
    - add number_format()
    - styling adjustments

--- templates/cartItem.php
+++ templates/cartItem.php
@@ -1,8 +1,11 @@
-<div class="col-4">
-    <img src="https://placekitten.com/286/180?image=<?= $cartItem['product_id'] ?>" class="card-img-top" alt="Produktbild-<?= $cartItem['product_id'] ?>">
+<div class="col-3">
+    <img class="productPicture" src="https://placekitten.com/306/192?image=<?= $cartItem['product_id'] ?>" class="card-img-top" alt="Produktbild-<?= $cartItem['product_id'] ?>">
 </div>
-<div class="col-8">
+<div class="col-7">
     <div><?= $cartItem['title'] ?></div>
     <div><?= $cartItem['description'] ?></div>
-    <div><?= $cartItem['price'] ?></div>
+    <div></div>
+</div>
+<div class="col-2 text-end">
+    <span class="price"></><?= number_format($cartItem['price']/100, 2, ',', ' ') ?> €</span>
 </div>
