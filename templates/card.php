210d394a670a11aa59a81fa0fa7a007fbab04aa5
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-07 16:44:22 +0200

    PHP Online Shop Part 5 by Vitalij Mic
    Button 'In den Warenkorb' jetzt mit Link und Funktion
    unterschiedliche Produktbilder über $row['id']

--- templates/card.php
+++ templates/card.php
@@ -1,6 +1,6 @@
 <div class="card">
     <div class="card-title"><?= $row['title'] ?></div>
-    <img src="https://placekitten.com/286/180" class="card-img-top" alt="Produktbild">
+    <img src="https://placekitten.com/286/180?image=<?= $row['id'] ?>" class="card-img-top" alt="Produktbild">
     <div class="card-body">
         <p><?= $row['description'] ?></p>
         <hr>
@@ -8,6 +8,6 @@
     </div>
     <div class="card-footer">
         <a href="" class="btn btn-primary btn-sm">Details</a>
-        <a href="" class="btn btn-success btn-sm">In den Warenkorb</a>
+        <a href="index.php/cart/add/<?= $row['id'] ?>" class="btn btn-success btn-sm">In den Warenkorb</a>
     </div>
 </div>
