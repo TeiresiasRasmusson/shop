5aab96e521a4c8456babc8eeecf8eb32d9ff87e9
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-09 23:14:20 +0200

    PHP Online Shop [Part 11] - by Vitalij Mic
    - basic layout created
    - section outsourced

---
+++ templates/selectDeliveryAddress.php
@@ -0,0 +1,12 @@
+<?php require_once __DIR__.'/header.php'; ?>
+
+<section class="container" id="newDeliveryAddress">
+    <?php require_once __DIR__.'/deliveryAddressForm.php'; ?>
+</section>
+
+<section class="container" id="selectDeliveryAddress">
+
+</section>
+
+
+<?php require_once __DIR__.'/footer.php'; ?>
