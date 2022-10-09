b3b2d9d58f99bc8f93800057444840b1dc5cc237
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-09 20:16:30 +0200

    PHP Online Shop [Part 9] - by Vitalij Mic
    - add function getUserDataForUsername()

--- functions/user.php
+++ functions/user.php
@@ -1,6 +1,6 @@
 <?php
 
-function getCurrentUserId(){
+function getCurrentUserId(): int{
 
     $userId = random_int(0,time());
 
@@ -11,4 +11,33 @@ function getCurrentUserId(){
         $userId = (int) $_SESSION['userId'];
     }
     return $userId;
+}
+
+function getUserDataForUsername(string $username): array{
+
+    $sql = "SELECT id, password
+            FROM user
+            WHERE username=:username";
+
+    $statement = getDB()->prepare($sql);
+    if(false === $statement){
+        return [];
+    }
+
+    $statement->execute(
+        [
+        ':username' => $username
+        ]
+    );
+
+    if(0 === $statement->rowCount()){
+        return [];
+    }
+    $row = $statement->fetch();
+    return $row;
+}
+
+function isLoggedIn(): bool{
+
+    return isset($_SESSION['userId']);
 }
