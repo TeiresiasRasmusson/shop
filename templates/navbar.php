7fc7693f793a5849d5524329846571439327d3ab
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-09 20:00:35 +0200

    PHP Online Shop [Part 10] - by Vitalij Mic
    - add login/logout functionality

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,7 +1,18 @@
 <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
         <a class="navbar-brand" href="#">My shop</a>
-        <ul class="navbar-nav ml-auto">
+        <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+                <?php if(isLoggedIn()): ?>
+                    <a class="nav-link" href="index.php/logout">Logout</a>
+                <?php endif; ?>
+                <?php if(!isLoggedIn()): ?>
+                    <a class="nav-link" href="index.php/login">Login</a>
+                <?php endif; ?>
+
+            </li>
+        </ul>
+        <ul class="navbar-nav">
             <li class="nav-item">
                 <a class="nav-link" href="index.php/cart">Warenkorb (<?= $countCartItems ?>)</a>
             </li>
