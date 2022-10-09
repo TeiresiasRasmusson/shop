eb5d02325cb3bd7b9e2ef723ba6f05753bcc3eee
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-09 19:58:43 +0200

    PHP Online Shop [Part 10] - by Vitalij Mic
    - add matching content from other templates

--- templates/header.php
+++ templates/header.php
@@ -1,1 +1,22 @@
 <?php
+/**
+* @var string $baseUrl
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <title>Kitten Webshop</title>
+    <base href="<?= $baseUrl ?>">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+<?php include __DIR__.'/navbar.php'?>
+<header class="bg-light pt-5 pb-5 mb-4">
+    <div class="container">
+        <h1>Willkommen im Kitten Webshop</h1>
+    </div>
+</header>
