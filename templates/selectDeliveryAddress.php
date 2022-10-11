c025278df3b14b4811f3989e66fb939050cc35c6
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-11 23:08:07 +0200

    PHP Online Shop [Part 13] - by Vitalij Mik
    - include /deliveryAddressList.php

--- templates/selectDeliveryAddress.php
+++ templates/selectDeliveryAddress.php
@@ -1,12 +1,11 @@
 <?php require_once __DIR__.'/header.php'; ?>
 
+<section class="container" id="selectDeliveryAddress">
+    <?php require_once __DIR__.'/deliveryAddressList.php'; ?>
+</section>
+
 <section class="container" id="newDeliveryAddress">
     <?php require_once __DIR__.'/deliveryAddressForm.php'; ?>
 </section>
-
-<section class="container" id="selectDeliveryAddress">
-
-</section>
-
 
 <?php require_once __DIR__.'/footer.php'; ?>
