e032db05337cbfc81a49dbd6965e9d0abcc5bb5e
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-11 20:40:46 +0200

    PHP Online Shop [Part 12] - by Vitalij Mik
    - add include for /functions/deliveryAddresses.php

--- includes.php
+++ includes.php
@@ -4,4 +4,5 @@ require_once __DIR__.'/functions/cart.php';
 require_once __DIR__.'/functions/user.php';
 require_once __DIR__.'/functions/product.php';
 require_once __DIR__.'/functions/utilities.php';
+require_once __DIR__.'/functions/deliveryAddresses.php';
 require_once __DIR__.'/routes.php';
