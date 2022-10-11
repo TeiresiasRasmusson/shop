6af013fb237b557be4bce5805105620f7f8b57fe
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-11 23:12:10 +0200

    PHP Online Shop [Part 13] - by Vitalij Mik
    - add basic layout for the deliveryAddressListe

--- templates/deliveryAddressList.php
+++ templates/deliveryAddressList.php
@@ -1,1 +1,26 @@
 <?php
+/**
+ * @var array $deliveryAddresses
+ */
+?>
+
+<div class="row pb-5">
+    <?php foreach ($deliveryAddresses as $deliveryAddress): ?>
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body">
+                    <strong class="recipient"><?= $deliveryAddress['recipient'] ?></strong>
+                    <p class="street">
+                        <?= $deliveryAddress['street'] ?> <?= $deliveryAddress['streetNumber'] ?>
+                    </p>
+                    <p class="city">
+                        <?= $deliveryAddress['zipCode'] ?> <?= $deliveryAddress['city'] ?>
+                    </p>
+                </div>
+                <div class="card-footer">
+                    <a class="card-link" href="index.php/selectDeliveryAddress/<?= $deliveryAddress['id'] ?>">Wählen</a>
+                </div>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>
