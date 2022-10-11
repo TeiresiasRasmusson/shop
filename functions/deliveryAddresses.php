d4e4ea22c85d45f4b526f5e43fbb14a461dc9bfa
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-11 20:40:08 +0200

    PHP Online Shop [Part 12] - by Vitalij Mik
    - add saveDeliveryAddressForUser ()

--- functions/deliveryAddresses.php
+++ functions/deliveryAddresses.php
@@ -1,1 +1,29 @@
 <?php
+
+function saveDeliveryAddressForUser (int $userId, string $recipient, string $street, string $streetNumber, string $city, string $zipCode) :int {
+
+    $sql = "INSERT INTO delivery_addresses
+    SET user_id = :userId,
+    recipient = :recipient,
+    street = :street,
+    streetNumber = :streetNumber,
+    city = :city,
+    zipCode = :zipCode";
+
+    $statement = getDB()->prepare($sql);
+
+    if(false === $statement){
+        return 0;
+    }
+
+    $statement->execute([
+        ':userId' => $userId,
+        ':recipient' => $recipient,
+        ':street' => &$street,
+        ':streetNumber' => $streetNumber,
+        ':city' => $city,
+        ':zipCode' => $zipCode
+    ]);
+
+    return (int)getDB()->lastInsertId();
+}
