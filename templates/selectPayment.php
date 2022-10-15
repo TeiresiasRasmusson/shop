787ee44b0a2615772618255e12c50fdd01425252
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-15 19:02:41 +0200

    PHP Online Shop [Part 14] - by Vitalij Mik
    - add section and form with radio-check and submit button

--- templates/selectPayment.php
+++ templates/selectPayment.php
@@ -1,1 +1,15 @@
-<?php
+<?php require_once __DIR__.'/header.php' ?>
+
+<section class="container" id="selectPaymentMethod">
+    <form method="POST" action="index.php/selectPayment">
+        <div class="form-check pb-2">
+            <label class="form-check-label" for="paymentPaypal">
+                PayPal
+            </label>
+            <input type="radio"class="form-check-input" name="paymentMethod" id="paypal" value="paypal">
+        </div>
+        <button type="submit" class="btn btn-primary">Weiter zu Bezahlung</button>
+    </form>
+</section>
+
+<?php require_once __DIR__.'/footer.php' ?>
