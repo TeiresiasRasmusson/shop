7f0cb5f3f472451fa31ac73b1598a5d3e784642d
Asmus Krause <asmus.krause@nur-fabrik.de>
2022-10-15 19:03:04 +0200

    PHP Online Shop [Part 14] - by Vitalij Mik
    - add function redirectIfNotLoggedIn()

--- functions/utilities.php
+++ functions/utilities.php
@@ -9,3 +9,13 @@ function escape(string $value): string{
 
     return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
 }
+
+function redirectIfNotLoggedIn(string $sourceTarget){
+
+    if(isLoggedIn()){
+        return;
+    }
+    $_SESSION['redirectTarget'] = BASE_URL.'index.php'. $sourceTarget;
+    header("Location: ". BASE_URL . "index.php/login");
+    exit();
+}
